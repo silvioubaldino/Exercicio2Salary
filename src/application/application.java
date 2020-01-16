babfc82a97fbd634dab71e627f9b928b1c858a90
Silvio Neto <silvioubaldino@gmail.com>
2020-01-16 09:07:23 -0400

    Exercício resolvido

---
+++ src/application/application.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class application {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Employee employee = new Employee();
+		
+		System.out.print("Nome: ");
+		employee.name = sc.nextLine();
+		System.out.print("Gross salary: ");
+		employee.grossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println("Employee: " + employee.name + ", " + employee.netSalary());
+		
+		System.out.print("Which percentage to increase salary? ");
+		double percentage = sc.nextInt();
+		employee.increaseSalary(percentage);
+		
+		System.out.println("Updated data: " + employee.name + ", " + employee.netSalary());
+		
+		sc.close();
+	}
+
+}
